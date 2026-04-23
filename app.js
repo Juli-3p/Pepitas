202ce2c703ecf00362a74bc67831a2bf2b8295ff
Juli-3p <jlucasantichia@gmail.com>
2026-04-23 19:52:26 -0300

    actualice index

--- app.js
+++ app.js
@@ -4,24 +4,13 @@ const app = express();
 
 const port = 3000;
 
-const user ={
-    primerNombre: "Julian",
-    apellido: "Santichia",
-    isAdmin: true,
-};
-
-const tasks =[
-    {title:"Aprender JS", description:"Revisar lo aprendido en PdeP"},
-    {title:"Aprender HTML", description:"Revisar lo aprendido en DW"},
-    {title:"Aprender CSS", description:"Revisar lo aprendido de CSS"},
-]
-
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get('/',(req,res) =>{
-    res.render("pages/index",{user, tasks});
+    res.render("../views/paginas/index");
 });
 
 app.listen(port, () =>{
-    console.log("Aplicacion funcionando en el puerto ${port}");
+    console.log("Aplicacion funcionando en el puerto");
 });
