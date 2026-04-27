dea89eff26c957b877a123ae12647b69f1054f9b
Rodrigo Martinez <rodrigoml24112004@gmail.com>
2026-04-27 20:49:43 -0300

    agregue rutas en app.js

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/',(req,res) =>{
-    res.render("../views/paginas/index");
-});
+app.get('/',(req,res) => res.render("../views/paginas/index")); //inicio
+
+app.get('/logeo',(req,res) => res.render("../views/paginas/logeo")); //login
+
+app.get('/registro',(req,res) => res.render("../views/paginas/registro")); //registro
+
+app.get('/carrito',(req,res) => res.render("../views/paginas/carrito")); //carrito
 
 app.listen(port, () =>{
     console.log("Aplicacion funcionando en el puerto");
