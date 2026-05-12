a5aa3144741ad04eb6d6d879f292656b04b7d831
Rodrigo Martinez <rodrigoml24112004@gmail.com>
2026-05-11 21:29:01 -0300

    correcciones en app,index
    :p

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ app.use(express.static("public"));
 
 app.get('/',(req,res) => res.render("../views/paginas/index")); //inicio
 
+app.post('/', (req, res) => {
+    res.redirect('/vistProd');
+});
+
 app.get('/logeo',(req,res) => res.render("../views/paginas/logeo")); //login
 
+app.post('/logeo', (req, res) => {
+    res.redirect('/');
+});
+
 app.get('/registro',(req,res) => res.render("../views/paginas/registro")); //registro
+
+app.post('/registro', (req, res) => {
+    res.redirect('/');
+});
 
 app.get('/carrito',(req,res) => res.render("../views/paginas/carrito")); //carrito
 
