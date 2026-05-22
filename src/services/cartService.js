14c3de47fcc03637eadfccf32261cb5288989055
Juli-3p <jlucasantichia@gmail.com>
2026-05-22 17:00:10 -0300

    user story 6

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,4 +1,4 @@
-const productos = require("../datos/products.json");
+const productsService = require("./productsService");
 
 const cartService = {
     initializeCart: (session) => {
@@ -6,71 +6,63 @@ const cartService = {
             session.cart = [];
         }
     },
-
     addProduct: (session, productId) => {
         cartService.initializeCart(session);
-        const productoEnCarrito =
-            session.cart.find(
-                item => item.productId === productId
-            );
+
+        const producto = productsService.getProductById(productId);
+        if (!producto) {
+            return false;
+        }
+        const productoEnCarrito = session.cart.find(item => item.productId === productId);
+
         if (productoEnCarrito) {
             productoEnCarrito.quantity++;
         } else {
             session.cart.push({
-                productId,
-                quantity: 1
+                productId,quantity: 1
             });
         }
+        return true;
     },
-
     decreaseProduct: (session, productId) => {
+
         cartService.initializeCart(session);
-        const productoEnCarrito =
-            session.cart.find(
-                item => item.productId === productId
-            );
+
+        const productoEnCarrito = session.cart.find(item => item.productId === productId);
+
         if (productoEnCarrito) {
             productoEnCarrito.quantity--;
             if (productoEnCarrito.quantity <= 0) {
-                session.cart =
-                    session.cart.filter(
-                        item => item.productId !== productId
-                    );
+                session.cart = session.cart.filter(item => item.productId !== productId);
             }
         }
     },
-    
     removeProduct: (session, productId) => {
+
         cartService.initializeCart(session);
-        session.cart =
-            session.cart.filter(
-                item => item.productId !== productId
-            );
+
+        session.cart = session.cart.filter(item => item.productId !== productId);
     },
-    
     clearCart: (session) => {
         session.cart = [];
     },
-    
     getCart: (session) => {
+
         cartService.initializeCart(session);
+
         return session.cart.map(item => {
-            const producto =
-                productos.find(
-                    p => Number(p.id) === item.productId
-                );
+            const producto = productsService.getProductById(item.productId);
             return {
                 ...producto,
                 quantity: item.quantity
             };
         });
     },
-    
     getTotal: (cart) => {
+
         return cart.reduce((acc, producto) => {
-            return acc + (
-                producto.price * producto.quantity
-            );
+
+            return acc + (producto.price * producto.quantity);
         }, 0);
     }
 };
