741d971f10b6c877296d212568d26c55330167d5
Giovanna M. <210918197+STARKgazing@users.noreply.github.com>
2026-05-21 21:39:43 -0300

    Update productRoute.js (Cambie ruta para que se vea el stock de producto)

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -6,7 +6,7 @@ const productController = require("../controllers/productController");
 router.get("/", productController.home);
 router.get("/categories/:category", productController.category);
 router.get("/products/:id", productController.detail);
-router.get("/vistProd", productController.vistProd);
+router.get("/vistProd", productController.detail);
 router.get("/:id", productController.detail);
 
 module.exports = router;
